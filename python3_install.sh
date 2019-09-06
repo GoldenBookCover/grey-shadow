a9fe3fc96c8dad8a3ba9401fb01087c17d24f187
Monstre Charmant <ballonrage@gmail.com>
2019-09-06 17:57:37 +0200

    Update python3_install.sh
    Formatting

--- python3_install.sh
+++ python3_install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 # Date - 2018-12-08
 # Author - CY
-# Function - install python3.7.2 
+# Function - install python3.7.4 on CentOS7
 ###########################################
 
 logfile="/home/$(basename ${0}).log"
-version='3.7.3'
-# 用来检验是否已安装
+version='3.7.4'
+# Just check if python3.7.4 is already installed
 install_path='/usr/local/lib/python3.7'
 
-# 显示版本并退出
+# Show version and exit
 if [ "x$1" = "x-v" ];then
 	echo "version $version" && exit 0
 fi
 
-# log file locates in /home/admin_tools
+# Create log folder if not existing
 if [ ! -d $(dirname "$logfile") ];then
-    mkdir -p $(dirname "$logfile") || exception '无法创建 $(dirname "$logfile") 目录'
+    mkdir -p $(dirname "$logfile") || exception 'Cannot create folder $(dirname "$logfile")'
 fi
-chown root:root $(dirname "$logfile")
-chmod 700 $(dirname "$logfile")
 
 # error handling
 function exception {
@@ -37,14 +35,14 @@ record "======Started at `date`======" || exception "Cannot create log file."
 ###########################################
 
 function python3_install {
-	yum install gcc gcc-c++ zlib zlib-devel libffi-devel openssl-devel bzip2-devel -y && record "安装依赖的环境" || exception "无法安装依赖的环境"
-	wget https://www.python.org/ftp/python/${version}/Python-${version}.tgz && tar zxf Python-${version}.tgz && record "获取 python3 源代码" || exception "获取 python3 源代码失败"
-	cd Python-${version} || exception "无法定位 python3 源代码"
-	./configure && record "configure 配置成功" || exception "configure 配置失败"
-	make && make install && record "安装成功" || exception "安装失败"
-	# python 安装好后，确认安装成功
-	record "查看 python3 版本"
-	record "可能需要重新登入终端，或开启一个新的 bash"
+	yum install gcc gcc-c++ zlib zlib-devel libffi-devel openssl-devel bzip2-devel -y && record "Install dependencies" || exception "Cannot install dependencies"
+	wget https://www.python.org/ftp/python/${version}/Python-${version}.tgz && tar zxf Python-${version}.tgz && record "Retriving python3 source code" || exception "Retriving python3 source code FAILED"
+	cd Python-${version} || exception "Cannot find python3 source code"
+	./configure && record "Configured" || exception "Configuration FAILED"
+	make && make install && record "Installation success" || exception "Installation FAILED"
+	# Check after installation
+	record "Check python3 version"
+	record "You may need to run a new terminal"
 	python3 --version 
 	sleep 2
 	# Python ${version} 
@@ -62,7 +60,7 @@ function startup {
 # Main function here.
 startup
 if [ -d "$install_path" ];then
-	record "$install_path 目录已存在"
+	record "$install_path directory already exists"
 else
 	python3_install	
 fi
